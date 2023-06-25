1ccec2ea3a410bcc0feb117014415c83a19f9aa3
reeteshmukul <reetesh.mukul@gmail.com>
2023-06-25 15:28:53 +0800

    Create runpod-isetup.sh

---
+++ runpod-isetup.sh
@@ -0,0 +1,11 @@
+apt-get update
+apt-get  install nano
+ln -sfn  /workspace/.vscode-server .vscode-server
+ln -sfn  /workspace/rmgit ~/rmgit
+cp  /workspace/.ssh/runpod*  ~/.ssh/
+cp /workspace/.profile ~
+cp /workspace/.bash_profile ~
+source ~/.profile
+source ~/.bash_profile
+chmod 400 ~/.ssh/runpod_id_rsa
+ssh-add ~/.ssh/runpod_id_rsa
