a4bc240ddecdc397f678da08d8550073b33e1ab3
reeteshmukul <reetesh.mukul@gmail.com>
2023-06-26 15:24:41 +0800

    Update runpod-isetup.sh

--- runpod-isetup.sh
+++ runpod-isetup.sh
@@ -2,12 +2,11 @@ if [[ "$EUID" = 0 ]];
  then 
   apt-get update
   apt-get  install nano
-  adduser --disabled-password --gecos "" rmukul
-  cp  /workspace/.ssh/runpod*  /home/rmukul/.ssh/
   chmod 400 ~/.ssh/runpod_id_rsa
 fi
 
 
+cp  /workspace/.ssh/runpod*  /home/rmukul/.ssh/
 ln -sfn  /workspace/.vscode-server /home/rmukul/.vscode-server
 ln -sfn  /workspace/rmgit /home/rmukul/rmgit
 cp /workspace/.profile /home/rmukul/
@@ -21,3 +20,4 @@ ssh-add /home/rmukul/.ssh/runpod_id_rsa
 
 git config --global user.email "reetesh.mukul@gmail.com"
 git config --global user.name "Reetesh Mukul"
+ 
